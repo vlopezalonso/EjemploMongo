3613fcb697ee3385e1c0643dd1631be6ccc3138c
Víctor probador <vlopezalonso@gmail.com>
2020-01-12 22:56:19 +0100

    subida primera

---
+++ EjemploMongoJPA/src/main/java/com/example/EjemploMongoJPA/EjemploMongoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.EjemploMongoJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploMongoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploMongoJpaApplication.class, args);
+	}
+
+}
---
+++ EjemploMongoJPA/src/test/java/com/example/EjemploMongoJPA/EjemploMongoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EjemploMongoJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploMongoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
