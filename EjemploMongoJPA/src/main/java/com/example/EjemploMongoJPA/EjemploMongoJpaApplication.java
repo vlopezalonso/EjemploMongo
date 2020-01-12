123e5846795bbb6b23c9d5c8a83eba5fa3af6eb6
Víctor probador <vlopezalonso@gmail.com>
2020-01-12 23:01:34 +0100

    s

--- EjemploMongoJPA/src/main/java/com/example/EjemploMongoJPA/EjemploMongoJpaApplication.java
+++ EjemploMongoJPA/src/main/java/com/example/EjemploMongoJPA/EjemploMongoJpaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EjemploMongoJpaApplication {
-  
+    
 	public static void main(String[] args) {
 		SpringApplication.run(EjemploMongoJpaApplication.class, args);
 	}
